3c28f2437d90731af032cd2d40cb6b639ab32382
dave@kapoq.com <dave@kapoq.com>
2012-04-30 13:57:41 +0100

    WIP

--- spec/lib/fozzie/inline_configuration_spec.rb
+++ spec/lib/fozzie/inline_configuration_spec.rb
@@ -56,27 +56,27 @@ end
 
 describe Fozzie::Inline do
 
-  class ExampleClass
-    include Fozzie::Inline
+  # class ExampleClass
+  #   include Fozzie::Inline
 
-    time_with_fozzie
-    def example_method
-      # TODO...
-    end
+  #   time_with_fozzie
+  #   def example_method
+  #     # TODO...
+  #   end
 
-    def example_method_with_fozzie_logging
-      # ??
-    end
+  #   def example_method_with_fozzie_logging
+  #     # ??
+  #   end
 
-  end
+  # end
 
-  let(:example) { ExampleClass.new }
+  # let(:example) { ExampleClass.new }
   let(:dummy_class) { Class.new { include Fozzie::Inline } }
 
-  it "logs the duration of a given method" do
-    S.expects(:time_for)
-    example.example_method
-  end
+  # it "logs the duration of a given method" do
+  #   S.expects(:time_for)
+  #   example.example_method
+  # end
   
   describe "#time_next_method_added?" do
     it "returns false by default" do
@@ -118,7 +118,7 @@ describe Fozzie::Inline do
   describe ".register_method" do
     it "sets up an alias method chain for the object with the given method name and the suffix '_without_fozzie_logging'" do
       dummy_class.expects(:alias_method_chain).with(:foo, :fozzie_logging)
-      dummy_class.send(:define_method, :foo, lambda {})
+      dummy_class.register_method(:foo)
     end
   end
 end
