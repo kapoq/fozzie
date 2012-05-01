16473f73c1eb92ba1fddb13cea32941a0aa44bc7
dave@kapoq.com <dave@kapoq.com>
2012-05-01 10:20:06 +0100

    WIP

---
+++ lib/fozzie/inline_configuration.rb
@@ -0,0 +1,55 @@
+module Fozzie
+  module Inline
+    def self.included(base)
+      base.class_eval { extend ClassMethods }
+    end
+
+    module ClassMethods
+      def time_with_fozzie
+        @time_next_method_added = true
+      end
+      
+      def time_next_method_added?
+        @time_next_method_added ||= false
+      end
+      
+      def method_added(*args)
+        register_method(*args) if time_next_method_added?
+      end
+
+      def register_method(m)
+        @time_next_method_added = false
+        alias_method_chain(m, :fozzie_logging) do |aliased_target, punctuation|
+          define_method("#{aliased_target}_with_fozzie_logging") do |*args|
+            S.time_for(m) do
+              send(aliased_target, *args)
+            end
+          end
+        end
+      end
+
+      # TODO: import from alias_method_chain
+      def alias_method_chain(target, feature)
+        # Strip out punctuation on predicates or bang methods since
+        # e.g. target?_without_feature is not a valid method name.
+        aliased_target, punctuation = target.to_s.sub(/([?!=])$/, ''), $1
+        yield(aliased_target, punctuation) if block_given?
+
+        with_method, without_method = "#{aliased_target}_with_#{feature}#{punctuation}", "#{aliased_target}_without_#{feature}#{punctuation}"
+
+        alias_method without_method, target
+        alias_method target, with_method
+
+        case
+          when public_method_defined?(without_method)
+            public target
+          when protected_method_defined?(without_method)
+            protected target
+          when private_method_defined?(without_method)
+            private target
+        end
+      end
+    end
+
+  end
+end
--- spec/lib/fozzie/inline_configuration_spec.rb
+++ spec/lib/fozzie/inline_configuration_spec.rb
@@ -1,82 +1,38 @@
 require 'spec_helper'
 
-module Fozzie
-  module Inline
-
-    def self.included(base)
-      base.class_eval { extend ClassMethods }
-    end
-
-    module ClassMethods
-
-      def time_with_fozzie
-        @time_next_method_added = true
-      end
-      
-      def time_next_method_added?
-        @time_next_method_added ||= false
-      end
-
-      def method_added(*args)
-        if time_next_method_added?
-          register_method(*args)
-        end
-      end
-
-      def register_method(m)
-        @time_next_method_added = false
-        alias_method_chain m, :fozzie_logging
-      end
-
-      # TODO: import from alias_method_chain
-      def alias_method_chain(target, feature)
-        # Strip out punctuation on predicates or bang methods since
-        # e.g. target?_without_feature is not a valid method name.
-        aliased_target, punctuation = target.to_s.sub(/([?!=])$/, ''), $1
-        yield(aliased_target, punctuation) if block_given?
-
-        with_method, without_method = "#{aliased_target}_with_#{feature}#{punctuation}", "#{aliased_target}_without_#{feature}#{punctuation}"
-
-        alias_method without_method, target
-        alias_method target, with_method
-
-        case
-          when public_method_defined?(without_method)
-            public target
-          when protected_method_defined?(without_method)
-            protected target
-          when private_method_defined?(without_method)
-            private target
-        end
-      end
-    end
-
-  end
-end
-
 describe Fozzie::Inline do
+  class ExampleClass
+    include Fozzie::Inline
 
-  # class ExampleClass
-  #   include Fozzie::Inline
+    time_with_fozzie
+    def self.example_class_method
+      # TODO...
+    end
+    
+    time_with_fozzie
+    def example_instance_method
+      # TODO...
+    end
 
-  #   time_with_fozzie
-  #   def example_method
-  #     # TODO...
-  #   end
+    def example_method_with_fozzie_logging
+      # ??
+    end
+  end
+  
+  let(:example) { ExampleClass.new }
+ 
+  it "logs the duration of a given instance method" do
+    S.expects(:time_for)
+    example.example_instance_method
+  end
 
-  #   def example_method_with_fozzie_logging
-  #     # ??
-  #   end
+  it "logs the duration of a given class method" do
+    S.expects(:time_for)
+    ExampleClass.example_class_method
+  end
 
-  # end
-
-  # let(:example) { ExampleClass.new }
+  
   let(:dummy_class) { Class.new { include Fozzie::Inline } }
-
-  # it "logs the duration of a given method" do
-  #   S.expects(:time_for)
-  #   example.example_method
-  # end
   
   describe "#time_next_method_added?" do
     it "returns false by default" do
@@ -116,7 +72,7 @@ describe Fozzie::Inline do
   end
 
   describe ".register_method" do
-    it "sets up an alias method chain for the object with the given method name and the suffix '_without_fozzie_logging'" do
+    it "sets up an alias method chain for the object with the given method name and the suffix :fozzie_logging" do
       dummy_class.expects(:alias_method_chain).with(:foo, :fozzie_logging)
       dummy_class.register_method(:foo)
     end
